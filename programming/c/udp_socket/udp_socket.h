faf6cc6381085d5b48e828cebdc476c21852b404
O. Walch <info@walcholi.ch>
2016-03-02 10:59:43 +0100

    Makefile added

---
+++ programming/c/udp_socket/udp_socket.h
@@ -0,0 +1,20 @@
+/**
+ * @brief	This module implements a simple UDP socket listener.
+ * @author	Oliver Walch
+ * @date	2016-03-02
+ */
+#ifndef UDP_SOCKET_H_
+#define UDP_SOCKET_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#define UDP_PORT	18264 	/* UDP port */
+#define BUFSIZE		2048	/* Incomming buffer size */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UDP_SOCKET_H_ */
